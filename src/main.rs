e0c250eef2ed09bf16f1155e38a0f689f032d534
choked-zzZ <azureskyemusic@gmail.com>
2025-11-02 18:04:09 +0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io::{self, Write};
 
 fn main() {
     let mut buffer = String::new();
-    // loop {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    buffer.pop();
-    println!("{}: command not found", buffer);
-    // }
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut buffer).unwrap();
+        buffer.pop();
+        println!("{}: command not found", buffer);
+    }
 }
